880d9042cc9fec7f66f14087b83f7e47e559aa03
Johan-py <johanenvdev@gmail.com>
2026-03-25 00:04:07 -0400

    add: config para vercel

---
+++ backend/.eslintcr.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    node: true,
+    es2022: true
+  }
+}
