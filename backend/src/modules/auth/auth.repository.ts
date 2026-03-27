73bb0c43a64f33dccead811a41bcf484e81de607
FranciscoKilibarda <panchitokili555@gmail.com>
2026-03-27 16:14:15 -0400

    Se a creado la tabla de usuario

--- backend/src/modules/auth/auth.repository.ts
+++ backend/src/modules/auth/auth.repository.ts
@@ -1,7 +1,40 @@
 import { prisma } from "../../lib/prisma.js";
 
+interface CreateUserInput {
+  nombre: string
+  apellido: string
+  correo: string
+  password: string
+  telefono?: string
+}
+
+export const createUser = async (data: CreateUserInput) => {
+  return await prisma.usuario.create({
+    data: {
+      nombre: data.nombre,
+      apellido: data.apellido,
+      correo: data.correo,
+      password: data.password,
+      rolId: 2,
+      telefonos: data.telefono
+        ? {
+            create: {
+              codigoPais: '+591',
+              numero: data.telefono,
+              principal: true
+            }
+          }
+        : undefined
+    },
+    include: {
+      telefonos: true
+    }
+  })
+}
+
 export const findUser = async (correo: string) => {
   return await prisma.usuario.findUnique({
     where: { correo },
   });
 };
+
