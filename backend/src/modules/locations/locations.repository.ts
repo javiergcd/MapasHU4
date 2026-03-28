18bab284206bacc69df0387ce3e2751757ced174
Miguel Angel Massi Geronimo <miguelangelmassigeronimo@gmail.com>
2026-03-28 08:32:42 -0400

    feat(buscadorHome): Se agrego logica de busqueda por municipio.

--- backend/src/modules/locations/locations.repository.ts
+++ backend/src/modules/locations/locations.repository.ts
@@ -10,11 +10,15 @@ export class LocationsRepository {
   async findByName(query: string) {
     return await prisma.ubicacion_maestra.findMany({
       where: {
-        nombre: { contains: query, mode: 'insensitive' },
+        OR: [
+          { nombre: { contains: query, mode: 'insensitive' } },//Esta es la zona porsiacaso en la bd esta como nombre
+          { municipio: { contains: query, mode: 'insensitive' } },
+        ],
       },
       select: {
         id: true,
         nombre: true,
+        municipio: true,
         departamento: true,
       },
       orderBy: { popularidad: 'desc' },
