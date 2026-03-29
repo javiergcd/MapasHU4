d46e136b1c1a2fd05aa3cbb279a06b86c18ebd7d
Esther Romero Menacho <romeromenachoesther@gmail.com>
2026-03-29 01:03:51 -0400

    feat(filtrosHomePage): registre endpoint de filtros para la homepage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express'
+import cors from 'cors';
+import { FiltersHomepageController } from './modules/filtershomepage/filtershomepage.controller.js';
 
 const app = express()
-
+app.use(cors());
 app.use(express.json())
+
+
+const filtersController = new FiltersHomepageController();
+app.get('/api/filters', filtersController.getFilters);
 
 // ✅ ENDPOINT
 app.post('/api/users', (req, res) => {
