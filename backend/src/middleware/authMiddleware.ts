d337d60a0bdbd99aba8bca4408a952deac1e0ea4
AlbertoQuispe478 <202103853@est.umss.edu>
2026-03-30 16:46:35 -0400

    fix:update

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,10 +1,10 @@
 // backend/src/middleware/auth.middleware.ts
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { env } from "../config/env.js"; // importa tu env.ts
+import { env } from "../config/env.js"; 
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers["authorization"]?.split(" ")[1]; // formato: Bearer <token>
+  const token = req.headers["authorization"]?.split(" ")[1]; 
 
   if (!token) {
     return res.status(401).json({ message: "No token provided" });
@@ -12,7 +12,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
 
   try {
     const decoded = jwt.verify(token, env.JWT_SECRET) as { userId: number };
-    (req as any).userId = decoded.userId; // guardamos el userId en la request
+    (req as any).userId = decoded.userId; 
     next();
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
